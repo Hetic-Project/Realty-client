c1915d410e6893159b8d9a4d89d27def169fd410
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-06-08 13:51:10 +0200

    Bug var

--- pages/partials/userHeader.php
+++ pages/partials/userHeader.php
@@ -1,8 +1,8 @@
 <div class="global-userHeader">
     <img src="../../images/RealtyIcon.svg" alt="Logo Realty">
     <form action="" method="get">
-        <input type="text" placeholder="Recherche">
-        <button></button>
+        <input type="text" placeholder="Recherche" class="global-inputHeader">
+        <button><img src="" alt=""></button>
     </form>
-    <button><img src="" alt=""></button>
+    <button class="global-buttonIcone"><img src="../../images/iconeProfile.svg" alt="Icone Profile" class="global-icone"></button>
 </div>
