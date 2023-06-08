f43935c3d4a25121b0c4781394e7ac9db70bca79
Rubens Bonnin <r_bonnin@hetic.eu>
2023-06-08 14:37:42 +0200

    same

--- pages/userspace/login.php
+++ pages/userspace/login.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
+<html lang="en">
 <html>
 
 <head>
     <title>Billetterie</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="styleUserSpace/styleLogin.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/index.css">
+    <title>Connexion - Realty</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800&display=swap" rel="stylesheet">
 </head>
 
