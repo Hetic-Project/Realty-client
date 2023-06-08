8c4c1be5b3ae518650bd6a8e0cbf0669c7467fb1
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-06-08 13:34:44 +0200

    FIRST

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
     <title>Realty</title>
 </head>
 <body>
+    <?php 
+        include "./pages/partials/userHeader.php";
+        include "./pages/partials/sider.php";
+        include "./pages/partials/locationContainer.php"; 
+    ?>
 </body>
 </html>
---
+++ pages/partials/userHeader.php
@@ -0,0 +1,8 @@
+<div class="global-userHeader">
+    <img src="../../images/RealtyIcon.svg" alt="Logo Realty">
+    <form action="" method="get">
+        <input type="text" placeholder="Recherche">
+        <button></button>
+    </form>
+    <button><img src="" alt=""></button>
+</div>
