79887e58658f286759138954d3ffa31ca80308fd
unknown <yalmanlucas@gmail.com>
2023-06-09 10:27:01 +0200

    added the container for the location page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <?php 
-        include "./pages/partials/userHeader.php";
+        // include "./pages/partials/userHeader.php";
         include "./pages/partials/sider.php";
         include "./pages/partials/locationContainer.php"; 
     ?>
