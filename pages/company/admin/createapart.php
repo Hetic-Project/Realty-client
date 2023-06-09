fcfa4479670fabcd42070147ac44c627370528ce
Nioctalü <a_boccheciampe@hetic.eu>
2023-06-09 10:59:25 +0200

    Update createapart

--- pages/company/admin/createapart.php
+++ pages/company/admin/createapart.php
@@ -8,8 +8,21 @@
     <title>Mettre un bien en vente - Realty</title>
 </head>
 <body>
-    <form>
-        <
+    <form class="formCreateApart">
+        <section class="containerAddPicture">
+            <div class="firstDragAndDrop">
+                <div class="addPicture">
+                    <img  class="iconePlusCreateApart" src="../../../images/iconePlus.svg">
+                    <p id="textAddPicture">Ajouter image</p>
+                </div>
+            </div>
+            <div class="typeOfAccommodation">
+                <input class="inputTypeOfAccommodation" type="text" placeholder="Type : Maison, Appartement....." id="name" name="name" required>
+            </div>
+            <div class="capacity">
+                <input class="inputCapacity" type="text" placeholder="Capaciter: 2 Voyageurs, 1 Chambre..." id="name" name="name" required>
+            </div>
+        </section>
     </form>
 </body>
 </html>
