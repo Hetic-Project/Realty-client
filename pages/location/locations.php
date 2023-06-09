0b3bf006316bab4dc6cbb8f75853e60f23df8e2f
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-06-09 12:08:34 +0200

    Location container v3

--- pages/location/locations.php
+++ pages/location/locations.php
@@ -8,9 +8,6 @@
     <title>Locations - Realty</title>
 </head>
 <body>
-<<<<<<< HEAD
-    
-=======
     <div class="dateContainer">
         <div class="dateSelectContainer departureDate">
             <img src="../../images/departSVG.svg" class="global-icon"></img>
@@ -26,30 +23,10 @@
         const today = new Date().toISOString().split('T')[0];
         document.getElementById('departureDate').value = today;
     </script>
+    <?php 
+        include "../partials/locationContainer.php";
 
-    <div class="global-mainContainer">
-    <a class="global-locationContainer" href="./locationdetails.php">
-            <img class="global-imgLocation" src="../../images/apartment1.svg" alt="appartement">
-                <div>
-                    <div class="global-textposition">
-                        <p class="global-title">Champ de Mars</p>
-                        <p class="global-description" display="grid">En entrant dans l'appartement, vous êtes accueilli(e) par un hall d'entrée spacieux et lumineux qui mène à toutes les pièces principales. Le salon est vaste, avec de hauts plafonds, des moulures et de grandes fenêtres qui laissent entrer une abondance de lumière naturelle. Il est aménagé de manière élégante, avec des meubles confortables, une cheminée et des œuvres d'art soigneusement choisies.</p>
-                        <p class="global-subtitle">829€ la nuit, 4983€ au total </p>
-                    </div>
-                </div>
-        </a>
-
-        <a class="global-locationContainer" href="./locationdetails.php">
-            <img class="global-imgLocation" src="../../images/apartment1.svg" alt="appartement">
-                <div>
-                    <div class="global-textposition">
-                        <p class="global-title">Champ de Mars</p>
-                        <p class="global-description" display="grid">En entrant dans l'appartement, vous êtes accueilli(e) par un hall d'entrée spacieux et lumineux qui mène à toutes les pièces principales. Le salon est vaste, avec de hauts plafonds, des moulures et de grandes fenêtres qui laissent entrer une abondance de lumière naturelle. Il est aménagé de manière élégante, avec des meubles confortables, une cheminée et des œuvres d'art soigneusement choisies.</p>
-                        <p class="global-subtitle">829€ la nuit, 4983€ au total </p>
-                    </div>
-                </div>
-        </a>
-    </div>
->>>>>>> 7c5467879e122491290be9c688b318f1fc03bd85
+        include "../partials/locationContainer.php";
+    ?>
 </body>
 </html>
