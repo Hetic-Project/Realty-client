17b1546e5d071299122b817b2be10096fe03cb5f
Vandal-William <vandal.william@hotmail.com>
2023-06-09 10:31:57 +0200

    connexion login sign in ok

--- pages/userspace/login.php
+++ pages/userspace/login.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Billetterie</title>
+    <title>Realty</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../../styles/index.css">
@@ -13,18 +13,22 @@
 
     <?php include "../../pages/partials/userHeader.php" ?>
 
-    <div class="cont">
+    <div class="containerLogin">
         <div class="form sign-in">
             <h2 class="h2Login">Sign In</h2>
-            <form method="post">
+
+            <form action="http://localhost:4000/user/loginAccount"  method="POST">
+
                 <label class="labelLogin">
                     <span class="spanLogin">Email</span>
-                    <input class="inputLogin" type="text" name="email" id="email" required>
+                    <input class="inputLogin" type="text" name="mail" id="email" required>
                 </label>
+
                 <label class="labelLogin">
                     <span class="spanLogin">Password</span>
-                    <input class="inputLogin" type="text" name="password" id="password" required>
+                    <input class="inputLogin" type="password" name="password" id="password" required>
                 </label>
+
                 <button type="submit" name="fformsend" id="fformsend" class="submit buttonLogin" required>Sign
                     In</button>
             </form>
@@ -32,7 +36,7 @@
             <p class="forgot-pass">Mot de passe oublié ?</p>
         </div>
 
-        <div class="sub-cont">
+        <div class="sub-containerLogin">
             <div class="img">
                 <div class="img-text m-up">
                     <h2 class="h2Login">Nouveau ici?</h2>
@@ -50,23 +54,28 @@
             <div class="form sign-up">
                 <h2 class="h2Login">Sign Up</h2>
 
-                <form method="post">
+                <form action="http://localhost:4000/user/createAccount" method="post">
                     <label class="labelLogin">
-                        <span class="spanLogin">Name</span>
-                        <inpu class="inputLogin" type="name" name="name" id="name" required>
+                        <span class="spanLogin">Prénom</span>
+                        <input class="inputLogin" type="text" name="firstname" id="name" required>
+                    </label>
+                    <label class="labelLogin">
+                        <span class="spanLogin">Nom</span>
+                        <input class="inputLogin" type="text" name="lastname" id="name" required>
+                    </label>
+                    <label class="labelLogin">
+                        <span class="spanLogin">Date de naissance</span>
+                        <input class="inputLogin" type="date" name="birthday" id="name" required>
                     </label>
                     <label class="labelLogin">
                         <span class="spanLogin">Email</span>
-                        <input class="inputLogin" type="email" name="Email" id="Email" required>
+                        <input class="inputLogin" type="email" name="mail" id="Email" required>
                     </label>
                     <label class="labelLogin">
-                        <span class="spanLogin">Password</span>
-                        <input class="inputLogin" type="password" name="Password" id="Password" required>
+                        <span class="spanLogin">Mot de Passe</span>
+                        <input class="inputLogin" type="password" name="password" id="Password" required>
                     </label>
-                    <label class="labelLogin">
-                        <span class="spanLogin">Confirm Password</span>
-                        <input class="inputLogin" type="text" name="confirm password" id="confirm password" required>
-                    </label>
+                
                     <button type="submit" name="formsend" id="formsend" class="submit buttonLogin" value="OK">Sign Up
                         Now</button>
                 </form>
