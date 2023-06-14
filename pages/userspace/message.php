9690a5e98cd01e3069a482eb9d7aece5dc1ff4d6
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-06-14 09:44:41 +0200

    message v1

--- pages/userspace/message.php
+++ pages/userspace/message.php
@@ -5,26 +5,79 @@ include "../partials/userHeader.php";
 ?>
 <body>
     <div class="msgLocationContainer">
-        <p class="msgMenuTitle">Locations</p>
+        <div class="slider">
+            <p class="msgMenuTitle">Message</p>
+    
+            <p class="msgApartmentName">Appartement 1</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
-        <p class="msgApartmentName">Appartement 1</p>
-        <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        
-        <p class="msgApartmentName">Appartement 2</p>
-        <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+            <p class="msgApartmentName">Appartement 2</p>
+            <p class="msgApartmentDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
     </div>
 
     <div class="messageContainer">
-        <div class="backgroundMessage">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aut sint maxime dolorem beatae! Modi optio consequuntur fugi</p>
-        </div>
+        <!-- <div class="allMessage">
+            <div class="slider">
+                <div class="backgroundMessage">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aut sint maxime dolorem beatae! Modi optio consequuntur fugi</p>
+                </div>
+                <div class="backgroundMessage">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aut sint maxime dolorem beatae! Modi optio consequuntur fugi</p>
+                </div>
+                <div class="backgroundMessage">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aut sint maxime dolorem beatae! Modi optio consequuntur fugi</p>
+                </div>
+            </div>
+        </div> -->
 
-        <div class="inputContainer">
+        <div class="allMessage">
+                <div class="transmiter">
+                    <p>hello !</p>
+                </div>
+
+                <div class="reciever">
+                    <p>z,ajodena zjna ello !</p>
+                </div>
+
+                <div class="transmiter">
+                    <p>hazl kodza odkj azidnaio znio dpaello !</p>
+                </div>
+
+                <div class="reciever">
+                    <p>hekazo llo !</p>
+                </div>
+
+
+        </div>
+    
+        <form class="inputContainer">
             <input class="msgInput" type="text" placeholder="Écrire un message...">
             <button class="sendButton">
                 Envoyer
             </button>
-        </div>
+        </form>
     </div>
 </body>
 </html>
