c614386c88d00ba26dc96a459641aec8b74da271
Rubens Bonnin <r_bonnin@hetic.eu>
2023-06-14 10:39:56 +0200

    profile v1

--- pages/userspace/profile.php
+++ pages/userspace/profile.php
@@ -3,6 +3,123 @@
 include "../partials/userHeader.php";
 
 ?>
-<body>
+
+<body class="bodyProfile">
+    <div class="navbarProfile">
+        <a href="#" class="active">Profil</a>
+        <a href="#">Mes Informations Personelles</a>
+        <a href="#">Mes locations</a>
+        <a href="#">Mes Factures</a>
+        <a href="#">Messages</a>
+        <a href="#">Sécurités et Confidentialités</a>
+        <a href="#" class>Deconnexion</a>
+    </div>
+    <div class="containerModifProfile">
+        <!-- <h1 class="titleProfile">Bonjour,</h1>
+        <h2 class="nameProfile">Rubens Bonnin</h2>é
+        <h3 class="titleProfile">Voyageur</h3> -->
+        <div class="profile">
+            <div class="profile-card">
+                <div class="frontProfile">
+                    <img class="" src="../../images/RealtyIcon.svg"></img>
+                    <h2 class="nameProfile">Rubens Bonnin</h2>
+                    <h3 class="titleProfileBot">Voyageur</h3>
+                </div>
+                <div class="backProfile">
+                    <div class="detailsProfile">
+                        <p class="pProfile"><strong>Prénom:</strong> Rubens</p>
+                        </br>
+                        <p class="pProfile"><strong>Nom:</strong> Bonnin</p>
+                        </br>
+                        <p class="pProfile"><strong>Date de Naissance:</strong> 21/06/2004</p>
+                        </br>
+                        <p class="pProfile"><strong>Téléphone:</strong> 0987654323</p>
+                        </br>
+                        <p class="pProfile"><strong>Ville:</strong> La Courneuve, Inde</p>
+                        </br>
+                        <p class="pProfile"><strong>Code Postal:</strong> 12678</p>
+                        </br>
+                        <p class="pProfile"><strong>Mot de Passe:</strong> 0é"'(§è!çà</p>
+                        </br>
+                        <p class="pProfile"><strong>Adresse Mail:</strong> exemple@gmail.com</p>
+                        </br>
+                        <p class="pProfile"><strong>Adresse:</strong> 10 rue Dunk</p>
+                        </br>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- <div class="containeProfile">
+            <div class="divlabelProfile">
+                <label class="labelProfile FirstNameProfile">
+                    <span class="spanProfile">Prénom</span>
+                    <input class="inputProfile" type="text" name="firstname" id="name" required>
+                </label>
+                <label class="labelProfile LastNameProfile">
+                    <span class="spanProfile">Nom</span>
+                    <input class="inputProfile" type="text" name="lastname" id="name" required>
+                </label>
+                <label class="labelProfile BirthdayProfile">
+                    <span class="spanProfile">Date de naissance</span>
+                    <input class="inputProfile" type="date" name="birthday" id="name" required>
+                </label>
+                <label class="labelProfile CityProfile">
+                    <span class="spanProfile">Ville</span>
+                    <input class="inputProfile" type="password" name="password" id="Password" required>
+                </label>
+                <label class="labelProfile PostalCodeProfile">
+                    <span class="spanProfile">Code Postal</span>
+                    <input class="inputProfile" type="password" name="password" id="Password" required>
+                </label>
+                <label class="labelProfile">
+                    <span class="spanProfiel">Email</span>
+                    <input class="inputProfile" type="email" name="mail" id="Email" required>
+                </label>
+                <label class="labelProfile">
+                    <span class="spanProfile">Mot de Passe</span>
+                    <input class="inputProfile" type="password" name="password" id="Password" required>
+                </label>
+
+                <button type="submit" name="formsend" id="formsend" class="submit buttonProfile"
+                    value="OK">Modifier</button>
+            </div>
+        </div> 
+    </div>-->
+        <script src="../../javascript/index.js"></script>
 </body>
+
 </html>
