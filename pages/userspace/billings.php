a6a6415d1841ad88eca1a55854b48d9424f0df72
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-06-15 15:19:52 +0200

    Billings v2

--- pages/userspace/billings.php
+++ pages/userspace/billings.php
@@ -6,16 +6,45 @@ include_once "../partials/navBarreProfile.php";
 ?>
 
 <body class="bodyProfile">
-    <div class="containerAllProfile">
-        <h2 class="titleProfile">Factures</h2>
+    <div class="navbarProfile">
+        <a href="#">Profil</a>
+        <a href="http://localhost:3000/pages/userspace/historyLocations.php">Mes locations</a>
+        <a href="#" class="active">Mes Factures</a>
+        <a href="http://localhost:3000/pages/userspace/message.php">Messages</a>
+        <a href="#">Sécurités et Confidentialités</a>
+        <a href="#">Déconnexion</a>
+        <a href="#">Désactiver le compte</a>
+    </div>
+
+    <div class="containerAllBillings">
+        <h2 class="titleProfile titleBillings">Factures</h2>
         
         <div class="allBillings">
-            <div class="slider">
+            <!-- <div class="ser"> -->
+                <div class="srollbillings"></div>
+                <div class="oneBilling">
+                    <hr class="reservationHr">
+                    <div class="contentBilling">
+                        <p><strong>Location :</strong><br> test 1</p>
+                            <div class="containerDate">
+                                <div class="dateBillings">
+                                    <img src="../../images/returnSVG.svg" class="global-icon">
+                                    <p>15/02/2023</p>
+                                </div>
+                                <div class="dateBillings">
+                                    <img src="../../images/departSVG.svg" class="global-icon">
+                                    <p>17/02/2023</p>
+                                </div>
+                            </div>
+                        <h4>890€</h4>
+                    </div>
+                    <hr class="reservationHr">
+                </div>
 
                 <div class="oneBilling">
                     <hr class="reservationHr">
                     <div class="contentBilling">
-                        <p><strong>Location :</strong><br> rue aznjji dnazj dn najzdi</p>
+                        <p><strong>Location :</strong><br> rue aznjji dnazj dn najzdi zejna z jan jan a</p>
                             <div class="containerDate">
                                 <div class="dateBillings">
                                     <img src="../../images/returnSVG.svg" class="global-icon">
@@ -183,26 +212,7 @@ include_once "../partials/navBarreProfile.php";
                     <hr class="reservationHr">
                 </div>
 
-                <div class="oneBilling">
-                    <hr class="reservationHr">
-                    <div class="contentBilling">
-                        <p><strong>Location :</strong><br> rue aznjji dnazj dn najzdi</p>
-                            <div class="containerDate">
-                                <div class="dateBillings">
-                                    <img src="../../images/returnSVG.svg" class="global-icon">
-                                    <p>15/02/2023</p>
-                                </div>
-                                <div class="dateBillings">
-                                    <img src="../../images/departSVG.svg" class="global-icon">
-                                    <p>17/02/2023</p>
-                                </div>
-                            </div>
-                        <h4>890€</h4>
-                    </div>
-                    <hr class="reservationHr">
-                </div>
-
-            </div>
+            <!-- </div> -->
         </div>
         
         
