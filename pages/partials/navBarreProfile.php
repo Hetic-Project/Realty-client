5c3e00c42f7aead27e84e18af82f47eaca2d62be
Nioctalü <a_boccheciampe@hetic.eu>
2023-06-15 14:07:09 +0200

    navBarreProfile.php

---
+++ pages/partials/navBarreProfile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="navbarProfile">
+        <a href="http://localhost:3000/pages/userspace/profile.php">Profil</a>
+        <a href="../location/locations.php" class="active">Mes locations</a>
+        <a href="http://localhost:3000/pages/userspace/billings.php">Mes Factures</a>
+        <a href="http://localhost:3000/pages/userspace/message.php">Messages</a>
+        <a href="#">Déconnexion</a>
+        <a href="#">Désactiver le compte</a>
+        <a href="#">Supprimer le compte</a>
+    </div>
+</body>
+</html>
