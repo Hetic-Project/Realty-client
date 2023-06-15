105823f10bc35e5968deac5a0c3ace298c4c122c
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-06-15 10:50:53 +0200

    jsp 2

--- pages/userspace/profile.php
+++ pages/userspace/profile.php
@@ -7,12 +7,12 @@ include "../partials/userHeader.php";
 <body class="bodyProfile">
     <div class="navbarProfile">
         <a href="#" class="active">Profil</a>
-        <a href="#">Mes Informations Personelles</a>
         <a href="#">Mes locations</a>
         <a href="#">Mes Factures</a>
         <a href="http://localhost:3000/pages/userspace/message.php">Messages</a>
         <a href="#">Sécurités et Confidentialités</a>
-        <a href="#" class>Deconnexion</a>
+        <a href="#">Déconnexion</a>
+        <a href="#">Désactiver le compte</a>
     </div>
 
     <div class="containerModifProfile">
