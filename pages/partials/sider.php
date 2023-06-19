e4f3308e77daabda2b212851617b69af8f02f782
TomuCard <120178316+TomuCard@users.noreply.github.com>
2023-06-19 10:42:39 +0200

    planning et autre

--- pages/partials/sider.php
+++ pages/partials/sider.php
@@ -18,7 +18,7 @@
 
         <a href="#" class="global-buttonHref"><img src="../../../images/iconDashboard.svg" alt="Icone Dashboard" class="global-icone">Dashboard</a>
         <a href="#" class="global-buttonHref"><img src="../../../images/iconMessage.svg" alt="Icone Message" class="global-icone">Message</a>
-        <a href="../../pages/company/employee/checklist.php" class="global-buttonHref"><img src="../../../images/iconChecklist.svg" alt="Icone Checklist" class="global-icone">Checklist</a>
+        <a href="../../company/employee/checklist.php" class="global-buttonHref"><img src="../../../images/iconChecklist.svg" alt="Icone Checklist" class="global-icone">Checklist</a>
         <a href="#" class="global-buttonHref"><img src="../../../images/iconPlanning.svg" alt="Icone Planning" class="global-icone">Planning</a>
         <a href="#" class="global-buttonHref"><img src="../../../images/iconStorage.svg" alt="Icone Storage" class="global-icone">Storage</a>
         <a href="#" class="global-buttonHref"><img src="../../../images/iconReport.svg" alt="Icone Report" class="global-icone">Report</a>
