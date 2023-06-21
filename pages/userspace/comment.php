7b3dadc3af265111285a5424fa6b30bb0eba31df
unknown <yalmanlucas@gmail.com>
2023-06-21 10:10:30 +0200

    review page added

---
+++ pages/userspace/comment.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "../partials/navBarreProfile.php";
+?>
+
+
+<body class="bodyProfile">
+    <div class="containerHistoryLocation">
+        <div class="commentContainer">
+            <p class="commentTitle">Faites-nous un retour sur votre expérience que vous avez eu avec notre service.</p>
+            <textarea name="" class="commentArea" placeholder="Écrire un commentaire"></textarea>
+            <button type="submit" class="reviewButton">Envoyer votre retour</button>
+        </div>
+    </div>
+</body>
