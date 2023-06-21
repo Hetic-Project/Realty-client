a99ef2c103dff702d1c62ba0fc9ae9387dd87bdf
ken william <theophanewilliam@gmail.com>
2023-06-21 11:28:56 +0200

    checklist.php | stylechecklist.php

--- pages/company/employee/checklist.php
+++ pages/company/employee/checklist.php
@@ -1,33 +1,56 @@
 <?php
-
 include "../../partials/sider.php";
-
 ?>
-    <div class="global-containerCompany">
 
-        <div class="checklist">
-            <h2 class="checklistTitle">Chambre</h2>
-            <div class="slider">
-                
-                <div class="checkbox-rect">
-                    <label for="1"> test 1 :</label>
-                    <input type="checkbox" name="1">
+<div class="global-containerCompany">
+    <div class="search-bar">
+        <input type="text" placeholder="Rechercher...">
+        <div class="search-options">
+            <div class="checkbox-option">
+                <input type="checkbox" id="Employe" name="Employe">
+                <label for="Employe">Rechercher un Utilisateur</label>
+            </div>
+            <div class="checkbox-option">
+                <input type="checkbox" id="Logistique" name="Logistique">
+                <label for="Logistique">Rechercher une location</label>
+            </div>
+        </div>
+        <button class="search-button">Envoyer</button>
+    </div>
+
+    <div class="checklist">
+        <!-- <h2 class="checklistTitle">Chambre</h2> -->
+        <div class="slider">
+            <div class="item">
+                <div class="user-image">
+                    <img src="../../../images/iconProfile.svg" alt="Image 1">
                 </div>
-                <div class="checkbox-rect">
-                    <label for="2">test 2 :</label>
-                    <input type="checkbox" name="2">
+                <div class="user-name">Utilisateur 1</div>
+                <button class="modify-button">Modifier</button>
+            </div>
+            <div class="item">
+                <div class="user-image">
+                    <img src="../../../images/iconProfile.svg" alt="Image 2">
                 </div>
-                <div class="checkbox-rect">
-                    <label for="3">test 3 :</label>
-                    <input type="checkbox" name="3">
+                <div class="user-name">Utilisateur 2</div>
+                <button class="modify-button">Modifier</button>
+            </div>
+            <div class="item">
+                <div class="user-image">
+                    <img src="../../../images/iconProfile.svg" alt="Image 3">
                 </div>
-                <div class="checkbox-rect">
-                    <label for="4">test 4 :</label>
-                    <input type="checkbox" name="4">
+                <div class="user-name">Utilisateur 3</div>
+                <button class="modify-button">Modifier</button>
+            </div>
+            <div class="item">
+                <div class="user-image">
+                    <img src="../../../images/iconProfile.svg" alt="Image 4">
                 </div>
-                
+                <div class="user-name">Utilisateur 4</div>
+                <button class="modify-button">Modifier</button>
             </div>
         </div>
     </div>
+</div>
 </body>
 </html>
