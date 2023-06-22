87d72a9d56d4c710e1148d431cc4a9881b67dadb
Nioctalü <a_boccheciampe@hetic.eu>
2023-06-22 09:28:44 +0200

    styleUpdateApart

--- pages/company/admin/updateapart.php
+++ pages/company/admin/updateapart.php
@@ -12,8 +12,27 @@
             </div>
         </div>
         <div class="reservationHr"></div>
-        
-
+        <div class="containerAddService">
+            <div class='Ratio42OUUUU?'></div>
+            <div class="buttonInput-FileUpdateApart">
+                <h3>Créer un service</h3>
+                <input class="buttonInputUpdateApart" type="file" id="file-input">
+                <label class="buttonInput-File" for="file-input">Modifier</label>
+            </div>
+        </div>
+        <div class="reservationHr"></div> 
+        <div class="agathaaaaa">
+            <h3>Commentaire :</h3>
+            <button>plus de commantaire</button>
+        </div>
+        <div class="containerComment">
+            <div class="comment">
+                <div>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+        </div>
     </form>
 </body>
 <script src="../../../javascript/createApart.js"></script>
