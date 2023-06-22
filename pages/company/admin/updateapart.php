db1025c6678ea3f83190653c3077167a01ef8c33
Nioctalü <a_boccheciampe@hetic.eu>
2023-06-22 12:05:37 +0200

    updateApart

--- pages/company/admin/updateapart.php
+++ pages/company/admin/updateapart.php
@@ -1,40 +1,94 @@
 <?php include "../../partials/sider.php" ?>
 <div class="global-containerCompany">
-    <form class="formUpdateApart">
+    <div class="containerUpdateApart">
         <div class="blabla">
             <h2>Rue d'Orset</h2>
             <h2>75001</h2>
         </div>
-        <div class="containerAddPicture">
+        <form class="containerAddPicture">
             <div id="image-container"></div>
             <div class="buttonInput-FileUpdateApart">
                 <input class="buttonInputUpdateApart" type="file" id="file-input"> 
                 <label class="buttonInput-File" for="file-input">Modifier</label>
             </div>
-        </div>
-        <div class="reservationHr"></div>
-        <div class="containerAddService">
+        </form>
+        <hr class="reservationHr">
+        <form class="containerAddService">
             <div class='Ratio42OUUUU?'></div>
             <div class="buttonInput-FileUpdateApart">
                 <h3>Créer un service</h3>
                 <input class="buttonInputUpdateApart" type="file" id="file-input">
                 <label class="buttonInput-File" for="file-input">Modifier</label>
             </div>
-        </div>
-        <div class="reservationHr"></div> 
-        <div class="agathaaaaa">
+        </form>
+        <hr class="reservationHr">
+        <div class="updateComment">
             <h3>Commentaire :</h3>
-            <button>plus de commantaire</button>
+            <a href="#" class="moreComment">Plus de commantaire</a>
         </div>
         <div class="containerComment">
-            <div class="comment">
-                <div>
-                    <button></button>
-                    <button></button>
+            <form class="oneComment">
+                <div class="contentOneComment">
+                    <strong>Sauver Willi</strong>
+                    <p>Mon commentaire...</p>
                 </div>
+                <div class="buttonOneComment">
+                    <button class="greenButtonOneComment">Valider</button>
+                    <button class="redButtonOneComment">Supprimer</button>
+                </div>
+            </form>
+            <form class="oneComment">
+                <div class="contentOneComment">
+                    <strong>Sauver Willi</strong>
+                    <p>Mon commentaire...</p>
+                </div>
+                <div class="buttonOneComment">
+                    <button class="greenButtonOneComment">Valider</button>
+                    <button class="redButtonOneComment">Supprimer</button>
+                </div>
+            </form>
+            <form class="oneComment">
+                <div class="contentOneComment">
+                    <strong>Sauver Willi</strong>
+                    <p>Mon commentaire...</p>
+                </div>
+                <div class="buttonOneComment">
+                    <button class="greenButtonOneComment">Valider</button>
+                    <button class="redButtonOneComment">Supprimer</button>
+                </div>
+            </form>
+            <form class="oneComment">
+                <div class="contentOneComment">
+                    <strong>Sauver Willi</strong>
+                    <p>Mon commentaire...</p>
+                </div>
+                <div class="buttonOneComment">
+                    <button class="greenButtonOneComment">Valider</button>
+                    <button class="redButtonOneComment">Supprimer</button>
+                </div>
+            </form>
+        </div>
+        <hr class="reservationHr">
+        <h3 class="planningName">Planning ménage :</h3>
+        <div class="employeUpdateApart">
+            <strong>Tomu Cardonnel</strong>
+            <a href="../../userspace/profile.php">Détails</a>
+        </div>
+        <div class="containerPlanningUpdateApart">
+            <div class="onePlanningUpdateApart">
+                <p class="contentPlanningUpdateApart">07/06/2023</p>
+                <button>Modifier</button>
+            </div>
+            <div class="onePlanningUpdateApart">
+                <p class="contentPlanningUpdateApart">07/06/2023</p>
+                <button>Modifier</button>
+            </div>
+            <div class="onePlanningUpdateApart">
+                <p class="contentPlanningUpdateApart">07/06/2023</p>
+                <button>Modifier</button>
             </div>
         </div>
-    </form>
+    </div>
 </div>
 </body>
 <script src="../../../javascript/createApart.js"></script>
